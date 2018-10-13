a7c8eb814561f95406a1f922d335f811b2fd2996
Kaushik Mishra <kaushik530411@gmail.com>
2018-10-13 17:47:30 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/myflashcard/MainActivity.java
@@ -0,0 +1,24 @@
+package com.codepath.myflashcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Kaushik","Flashcard question Clicked");
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
